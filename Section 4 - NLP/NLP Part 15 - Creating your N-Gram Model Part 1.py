65747f7a662fd0c6c81844e0aef24ff354e59f92
Bijoyan Das <bthesource@gmail.com>
2018-04-11 20:27:36 +0530

    Updating NLP

--- Section 4 - NLP/NLP Part 15 - Creating your N-Gram Model Part 1.py
+++ Section 4 - NLP/NLP Part 15 - Creating your N-Gram Model Part 1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Natural Language Processing using Python
 
 # N-Gram Modelling - Character Grams
@@ -31,40 +30,5 @@ for i in range(100):
     nextItem = possibilities[random.randrange(len(possibilities))]
     result += nextItem
     currentGram = result[len(result)-n:len(result)]
-    
-=======
-# Natural Language Processing using Python
-
-# N-Gram Modelling - Character Grams
-# Importing libraries
-import random
-
-# Sample data
-text = """Global warming or climate change has become a worldwide concern. It is gradually developing into an unprecedented environmental crisis evident in melting glaciers, changing weather patterns, rising sea levels, floods, cyclones and droughts. Global warming implies an increase in the average temperature of the Earth due to entrapment of greenhouse gases in the earth’s atmosphere."""
-
-# Order of the grams
-n = 2
-
-# Our N-Grams
-ngrams = {}
-
-# Creating the model
-for i in range(len(text)-n):
-    gram = text[i:i+n]
-    if gram not in ngrams.keys():
-        ngrams[gram] = []
-    ngrams[gram].append(text[i+n])
-     
-# Testing our N-Gram Model    
-currentGram = text[0:n]
-result = currentGram
-for i in range(100):
-    if currentGram not in ngrams.keys():
-        break;
-    possibilities = ngrams[currentGram]
-    nextItem = possibilities[random.randrange(len(possibilities))]
-    result += nextItem
-    currentGram = result[len(result)-n:len(result)]
-    
->>>>>>> 246cd984d0320eff2e8c481c12715f13590b4429
+  
 print(result)
